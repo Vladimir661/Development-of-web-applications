26fb5e651ad4ead40fe29684b14c1b6c09719c2d
Volodymyr Zozulia <xphigota@gmail.com>
2026-04-16 17:49:32 +0300

    main

---
+++ lab8/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="uk">
+<head>
+    <meta charset="UTF-8">
+    <title>HTML5 Бронювання кімнат в готелі (JavaScript/PHP/MySQL)</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js" type="text/javascript"></script>
+    <script src="js/daypilot-all.min.js" type="text/javascript"></script>
+</head>
+<body>
+<header>
+    <div class="bg-help">
+    <div class="inBox">
+        <h1 id="logo">HTML5 Бронювання кімнат в готелі (JavaScript/PHP)</h1>
+        <p id="claim">AJAX'овий Календар-застосунок з JavaScript/HTML5/jQuery</p>
+        <hr class="hidden" />
+    </div>
+    </div>
+</header>
+<main>
+    <div class="sidebar">
+    <div id="nav">тут будемо вставляти календар з бронюванням</div>
+    </div>
+</main>
+<div class="clear"></div>
+<footer>
+    <address>(с) Автор лабораторної роботи: студент спеціальності КН, Зозуля В.В.</address>
+</footer>
+</body>
+</html>
